fd15def701e7fd1acaeaf6740896aa1714f13e44
Phil Ting <pting.me+gh@gmail.com>
2022-10-25 17:04:17 -0700

    readded main.css

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,8 @@ import { render } from 'react-dom';
 import AppContainer from './AppContainer';
 import store from './app/store';
 import { Provider } from 'react-redux';
-
 import { worker } from './api/server';
+import './main.css';
 
 // Wrap app rendering so we can wait for the mock API to initialize
 async function start() {
