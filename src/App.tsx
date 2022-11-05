57b3f1cc202ce7b0588208238f5b24535199046c
Phil Ting <pting.me+gh@gmail.com>
2022-11-04 17:07:38 -0700

    renamed to single post page

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import { Navbar } from './app-lib/Navbar';
 import { AddPostForm } from './features/posts/AddPostForm';
 import { EditPostForm } from './features/posts/EditPostForm';
 import { PostsList } from './features/posts/PostsList';
-import { SinglePostPage } from './features/posts/SimplePostPage';
+import { SinglePostPage } from './features/posts/SinglePostPage';
 
 const App: FC = () => {
   return (
